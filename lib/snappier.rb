14a18646307962da75fa4d657632e0453aaba8e4
Mark Ryall <mark@fresho.com>
2025-05-25 12:36:51 +1000

    initial commit

---
+++ lib/snappier.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "snappier/version"
+
+module Snappier
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/snappier/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Snappier
+  VERSION = "0.1.0"
+end
